67a3838dddd0acaa0cdc57ab6d093fa5264a842b
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-08-03 22:57:05 +0100

    Update pt.py

--- pt.py
+++ pt.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import subprocess
 import edge_tts
+from gtts import gTTS
 
 
 
-st.write("""## TTS PT
+st.write("""## TTS anything
         
         """)
 
@@ -15,6 +16,20 @@ txt = st.text_input('Escreva: ')
 #     txt = listen_mic()
 #     st.write(txt)
 
-subprocess.run(['edge-tts' ,'--voice', 'pt-BR-AntonioNeural', '--text', txt, '--write-media', 'txt.mp3', '--write-subtitles', 'sub.vtt'])
+# subprocess.run(['edge-tts' ,'--voice', 'pt-BR-AntonioNeural', '--text', txt, '--write-media', 'txt.mp3', '--write-subtitles', 'sub.vtt'])
+
+#st.write(generated_answer)
+
+
+
+# Texto que você deseja converter em fala
+#texto = "Olá, isso é um exemplo de texto para fala usando o Google TTS com Python."
+
+# Cria um objeto gTTS
+tts = gTTS(text=txt, lang='pt', tld='com.br')
+
+# Salva o arquivo de áudio
+tts.save("txt.mp3")
+
 
 st.audio('txt.mp3')
